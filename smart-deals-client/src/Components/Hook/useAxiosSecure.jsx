d935a4f62f3caa4653036fb9d7fcb69549337098
sohagroy7223 <freelancersohagroy@gmail.com>
2026-06-27 22:23:17 +0600

    addad interceptor responce if user get status 401 or 403 , this user automatic log outin login page

--- smart-deals-client/src/Components/Hook/useAxiosSecure.jsx
+++ smart-deals-client/src/Components/Hook/useAxiosSecure.jsx
@@ -1,22 +1,42 @@
 import axios from "axios";
 import { AuthContext } from "../../Context/AuthContext";
 import { use, useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const instance = axios.create({
   baseURL: "http://localhost:3000",
 });
 const useAxiosSecure = () => {
-  const { user } = use(AuthContext);
+  const { user, logOut } = use(AuthContext);
+  const navigate = useNavigate();
   useEffect(() => {
     //request interceptor
     const requestInterceptor = instance.interceptors.request.use((config) => {
-      config.headers.Authorization = `bearer ${user.accessToken}`;
+      const token = user.accessToken;
+      if (token) {
+        config.headers.Authorization = `bearer ${token}`;
+      }
       return config;
     });
+
+    instance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        const status = err.status;
+        if (status === 401 || status === 403) {
+          logOut().then(() => {
+            navigate("/login");
+          });
+        }
+      },
+    );
+
     return () => {
       instance.interceptors.request.eject(requestInterceptor);
     };
-  }, [user]);
+  }, [user, navigate, logOut]);
 
   return instance;
 };
